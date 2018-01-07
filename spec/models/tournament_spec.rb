45820d5e983a0d7a7b1ff6191f98ac094394d070
Roman Serikov <roman.serikov@yandex.ru>
2018-01-07 18:17:43 +0300

    Add: specs for adding and removing teams from tournament

--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -15,4 +15,28 @@ RSpec.describe Tournament, type: :model do
     it { should validate_presence_of(:start_date) }
     it { should validate_presence_of(:end_date) }
   end
+
+  describe '#add_team' do
+    let(:tournament) { create(:tournament) }
+    let(:team_1) { create(:team) }
+
+    it 'adds team to tournament' do
+      tournament.add_team(team_1.id)
+
+      expect(tournament.teams).to eq [team_1]
+    end
+  end
+
+  describe '#remove_team' do
+    let(:tournament) { create(:tournament) }
+    let(:team_1) { create(:team) }
+    let(:team_2) { create(:team) }
+
+    it 'removes team from tournament' do
+      tournament.teams << team_1 << team_2
+      tournament.remove_team(team_1.id)
+
+      expect(tournament.teams).to eq [team_2]
+    end
+  end
 end
