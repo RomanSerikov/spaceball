e76f6940a46687972f6617997b6df8528656427b
Roman Serikov <roman.serikov@yandex.ru>
2018-01-15 20:22:18 +0300

    Fix: match validation

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,8 +4,8 @@ class Match < ApplicationRecord
   belongs_to :team_b, class_name: 'Team'
 
   validates :start_date, :start_time, presence: true
-  validates :team_a_goals, inclusion: { in: (0..50).to_a << nil }
-  validates :team_b_goals, inclusion: { in: (0..50).to_a << nil }
+  validates :team_a_goals, inclusion: { in: (0..50), allow_blank: true }
+  validates :team_b_goals, inclusion: { in: (0..50), allow_blank: true }
   validate :check_teams_are_different
 
   def title
