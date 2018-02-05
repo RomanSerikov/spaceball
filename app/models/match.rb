51a076dafa47bf7b77da26555a0650837b1ca546
Roman Serikov <roman.serikov@yandex.ru>
2018-02-05 22:55:25 +0300

    Improve users tournament interface

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,6 @@
 class Match < ApplicationRecord
+  default_scope { order(start_date: :asc) }
+
   belongs_to :tournament
   belongs_to :team_a, class_name: 'Team'
   belongs_to :team_b, class_name: 'Team'
